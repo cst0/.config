23e6aa477929045387312fa6d497d32302f12313
Chris Thierauf <christopher.thierauf@tufts.edu>
2021-06-13 15:59:43 -0400

    background image pathing and monitor selection

--- i3/scripts/monitor_selector.sh
+++ i3/scripts/monitor_selector.sh
@@ -5,9 +5,11 @@ chosen=$(echo -e "[Cancel]\nLaptop\nMonitor\nDocked" | rofi -dmenu -i)
 # https://www.freedesktop.org/software/systemd/man/systemd-sleep.conf.html#Description
 
 if [[ $chosen = "Laptop" ]]; then
-	~/.screenlayout/notdocked.sh
+	~/.config/screenlayout/notdocked.sh
 elif [[ $chosen = "Left Monitor" ]]; then
-	~/.screenlayout/left_monitor.sh
+	~/.config/screenlayout/left_monitor.sh
 elif [[ $chosen = "Docked" ]]; then
-    ~/.screenlayout/newestdocked.sh
+    ~/.config/screenlayout/newestdocked.sh
 fi
+
+nitrogen --set-zoom-fill ~/.config/i3/images/background.png
