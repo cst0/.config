5710e3c0ee42a6979b9e5d24385e5ea52db42956
Chris Thierauf <christopher.thierauf@tufts.edu>
2021-07-11 20:59:58 -0400

    cleanup fishrc

--- fish/config.fish
+++ fish/config.fish
@@ -5,20 +5,10 @@ end
 set PATH $PATH /home/cst/.config/coc/extensions/coc-clangd-data/install/11.0.0/clangd_11.0.0/bin
 set PATH $PATH /home/cst/.cargo/bin
 
-source /opt/rosactive/sourceable.fish
+#set ROS_PACKAGE_VERSION 3
+#source /opt/rosactive/sourceable.fish
+bass source ws_melodic_py3/install/setup.bash
 
 #source /opt/ros/melodic/share/rosbash/rosfish
 #bass source /opt/ros/melodic/setup.bash
-#
-#bass source ~/catkin_ws/devel/setup.bash
-#bass source ~/ws_acin/devel/setup.bash
-#bass source ~/ws_covid/devel/setup.bash
-#bass source ~/ws_enbarr/devel/setup.bash
-#bass source ~/ws_gecko/devel/setup.bash
-#bass source ~/ws_gpiocontrol/devel/setup.bash
-#bass source ~/ws_kinova/devel/setup.bash
-#bass source ~/ws_scratchpad/devel/setup.bash
-#bass source ~/ws_roscontrol/devel/setup.bash
-#bass source ~/ws_husky/devel/setup.bash
-#
 #set ROS_WORKSPACE /home/cst/ws_laserutils
