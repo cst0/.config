802afd2d7bf674e07db31f2d2f181c4dc9ca24d9
Chris Thierauf <christopher.thierauf@tufts.edu>
2021-11-20 14:01:20 -0500

    add monitor selection options

--- i3/scripts/monitor_selector.sh
+++ i3/scripts/monitor_selector.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-chosen=$(echo -e "[Cancel]\nLaptop\nMonitor\nDocked" | rofi -dmenu -i)
+chosen=$(echo -e "[Cancel]\nLaptop\nLeft Monitor (4k vertical)\nLeft Monitor (standard)\nDocked" | rofi -dmenu -i)
 # Info about some states are available here:
 # https://www.freedesktop.org/software/systemd/man/systemd-sleep.conf.html#Description
 
 if [[ $chosen = "Laptop" ]]; then
 	~/.config/screenlayout/notdocked.sh
-elif [[ $chosen = "Left Monitor" ]]; then
+elif [[ $chosen = "Left Monitor (4k vertical)" ]]; then
+	~/.config/screenlayout/vertical_4k.sh
+elif [[ $chosen = "Left Monitor (standard)" ]]; then
 	~/.config/screenlayout/left_monitor.sh
 elif [[ $chosen = "Docked" ]]; then
     ~/.config/screenlayout/newestdocked.sh
