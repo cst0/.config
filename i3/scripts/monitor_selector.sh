49df40f56b3608487bbaede1269215ed62394294
Chris Thierauf <christopher.thierauf@tufts.edu>
2021-11-29 13:46:35 -0500

    new graphics driver, new monitor script

--- i3/scripts/monitor_selector.sh
+++ i3/scripts/monitor_selector.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-chosen=$(echo -e "[Cancel]\nLaptop\nLeft Monitor (4k vertical)\nLeft Monitor (standard)\nDocked" | rofi -dmenu -i)
+chosen=$(echo -e "[Cancel]\nLaptop\nRight Monitor (standard)\nLeft Monitor (4k vertical)\nLeft Monitor (standard)\nDocked" | rofi -dmenu -i)
 # Info about some states are available here:
 # https://www.freedesktop.org/software/systemd/man/systemd-sleep.conf.html#Description
 
 if [[ $chosen = "Laptop" ]]; then
 	~/.config/screenlayout/notdocked.sh
+elif [[ $chosen = "Right Monitor (standard)" ]]; then
+	~/.config/screenlayout/vertical_4k.sh
 elif [[ $chosen = "Left Monitor (4k vertical)" ]]; then
 	~/.config/screenlayout/vertical_4k.sh
 elif [[ $chosen = "Left Monitor (standard)" ]]; then
