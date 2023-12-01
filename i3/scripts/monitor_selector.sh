e5365f8234ce4c3535b6a55d5a2d03132d34a039
Chris Thierauf <christopher.thierauf@whoi.edu>
2023-12-01 09:43:07 -0500

    extension and i3

--- i3/scripts/monitor_selector.sh
+++ i3/scripts/monitor_selector.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-chosen=$(echo -e "[Cancel]\nLaptop\nRight Monitor (standard)\nLeft Monitor (4k vertical)\nLeft Monitor (standard)\nDocked" | rofi -dmenu -i)
+chosen=$(echo -e "[Cancel]\nLaptop\nRight Monitor (standard)\nLeft Monitor (4k vertical)\nLeft Monitor (standard)\nHome Docked\nDocked" | rofi -dmenu -i)
 # Info about some states are available here:
 # https://www.freedesktop.org/software/systemd/man/systemd-sleep.conf.html#Description
 
@@ -12,8 +12,11 @@ elif [[ $chosen = "Left Monitor (4k vertical)" ]]; then
 	~/.config/screenlayout/vertical_4k.sh
 elif [[ $chosen = "Left Monitor (standard)" ]]; then
 	~/.config/screenlayout/left_monitor.sh
+elif [[ $chosen = "Home Docked" ]]; then
+    ~/.config/screenlayout/docked_home.sh
 elif [[ $chosen = "Docked" ]]; then
     ~/.config/screenlayout/newestdocked.sh
 fi
 
+sleep 5
 nitrogen --set-zoom-fill ~/.config/i3/images/background.png
